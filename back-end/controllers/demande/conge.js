de09ca3897b2e2525bdd427cee7d0bdbd6c57c4b
Ahmed-Laaziz <101425997+Ahmed-Laaziz@users.noreply.github.com>
2024-01-05 23:23:01 +0100

    Update conge.js

--- back-end/controllers/demande/conge.js
+++ back-end/controllers/demande/conge.js
@@ -20,3 +20,31 @@ exports.addDemandeConge = async (req, res, next) => {
       res.status(500).json({ error: 'Failed to add demande' });
     }
   };
+
+
+  exports.updateDemandeConge = async (req, res, next) => {
+    const demandeId = req.params.demandeId; // Supposons que vous recevez l'ID de la demande à mettre à jour dans les paramètres de la requête
+  
+    try {
+      const updatedDemande = await DemandeConge.findByIdAndUpdate(
+        demandeId,
+        {
+          $set: {
+            description: req.body.description,
+            de_date: req.body.de_date,
+            a_date: req.body.a_date,
+          },
+        },
+        { new: true } // Pour retourner le nouveau document mis à jour
+      );
+  
+      if (!updatedDemande) {
+        return res.status(404).json({ error: 'Demande not found' });
+      }
+  
+      res.status(200).json(updatedDemande);
+    } catch (error) {
+      console.error('Error updating demande:', error);
+      res.status(500).json({ error: 'Failed to update demande' });
+    }
+  };
