da8492c93a9cf22da8e468922832b8b31c4c7e01
Ahmed-Laaziz <101425997+Ahmed-Laaziz@users.noreply.github.com>
2024-01-05 23:23:45 +0100

    Update conge.js

--- back-end/routes/demande/conge.js
+++ back-end/routes/demande/conge.js
@@ -4,6 +4,6 @@ const demandeController = require("../../controllers/demande/conge");
 
 
 router.post('/add-demande-conge', demandeController.addDemandeConge);
-
+router.put('/update-demand-conge/:demandeId', demandeController.updateDemandeConge);
 
 module.exports = router;
